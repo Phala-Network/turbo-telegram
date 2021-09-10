63d70ce38e993bb5f582424350a0ff58be541236
amphineko <sukareki@gmail.com>
2021-09-11 00:48:54 +0100

    chore(config): drop influx configs
    Signed-off-by: amphineko <sukareki@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,17 @@
 import { Injectable, Module } from '@nestjs/common'
 import { IIngestorConfiguration as IIngestorConfig } from '../ingestors/config'
-import { IInfluxDBConfig } from '../storage/config'
 import { load } from './loaders'
 
 export interface IAppConfig {
-    readonly influxdb: IInfluxDBConfig
     readonly ingestors: IIngestorConfig
 }
 
 @Injectable()
 export class AppConfigService implements IAppConfig {
-    public readonly influxdb: IInfluxDBConfig
     public readonly ingestors: IIngestorConfig
 
     constructor() {
-        const { influxdb, ingestors } = load()
-        this.influxdb = influxdb
+        const { ingestors } = load()
         this.ingestors = ingestors
     }
 }
--- src/storage/config.ts
+++ src/storage/config.ts
@@ -1,20 +1,6 @@
 import { Injectable } from '@nestjs/common'
-import { AppConfigService } from '../config'
-
-export interface IInfluxDBConfig {
-    readonly bucket: string
-    readonly nodename: string
-    readonly org: string
-    readonly token: string
-    readonly url: string
-}
 
 @Injectable()
 export class StorageConfig {
-    public readonly influxdb: IInfluxDBConfig
-
-    constructor(config: AppConfigService) {
-        const { influxdb } = config
-        this.influxdb = influxdb
-    }
+    constructor(/* config: AppConfigService */) {}
 }
