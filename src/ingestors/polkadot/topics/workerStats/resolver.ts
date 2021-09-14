e92b5a4929bad0c3d9684cbee23ca7718063d197
amphineko <sukareki@gmail.com>
2021-09-14 23:56:17 +0100

    fix: request workerStatsPerHour should have chain name
    Signed-off-by: amphineko <sukareki@gmail.com>

--- src/ingestors/polkadot/topics/workerStats/resolver.ts
+++ src/ingestors/polkadot/topics/workerStats/resolver.ts
@@ -2,12 +2,23 @@ import { Args, Query, Resolver } from '@nestjs/graphql'
 import { WorkerStat } from './entities'
 import { BlockWorkerStatStorage } from './storage'
 
+const defaultHourLimit = 7 * 24
+const maxHourLimit = 7 * 24
+
 @Resolver(WorkerStat)
 export class BlockWorkerStatResolver {
     constructor(private readonly storage: BlockWorkerStatStorage) {}
 
     @Query(() => [WorkerStat])
-    async workerStatsPerHour(@Args('publicKey') publicKey: string): Promise<WorkerStat[]> {
-        return await this.storage.findByPublicKeyPerHour(publicKey)
+    async workerStatsPerHour(
+        @Args({ description: 'Public key of worker to be queried', name: 'publicKey' }) publicKey: string,
+        @Args({ description: 'Chain name to be queried', name: 'chainName' }) chainName: string,
+        @Args({ defaultValue: defaultHourLimit, description: 'Hours of history to be queried', name: 'limit' })
+        limit: number
+    ): Promise<WorkerStat[]> {
+        if (limit < 0 || limit > maxHourLimit) {
+            throw new Error('Limit is out of range')
+        }
+        return await this.storage.findByPublicKeyPerHour(publicKey, chainName, limit)
     }
 }
--- src/ingestors/polkadot/topics/workerStats/storage.ts
+++ src/ingestors/polkadot/topics/workerStats/storage.ts
@@ -7,12 +7,13 @@ import { WorkerStat } from './entities'
 export class BlockWorkerStatStorage {
     constructor(@InjectRepository(WorkerStat) private readonly repository: Repository<WorkerStat>) {}
 
-    public async findByPublicKeyPerHour(publicKey: string, limit?: number): Promise<WorkerStat[]> {
+    public async findByPublicKeyPerHour(publicKey: string, name: string, limit?: number): Promise<WorkerStat[]> {
         const query = this.repository
             .createQueryBuilder('record')
             .distinctOn([`date_trunc('hour', "blockTimestamp")`])
             .orderBy(`date_trunc('hour', "blockTimestamp")`, 'DESC')
             .limit(limit)
+            .where('record.chainName = :chainName', { chainName: name })
             .where('record.publicKey = :publicKey', { publicKey })
         return await query.getMany()
     }
